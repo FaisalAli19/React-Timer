f48a3b3f6ece2164a139b80098f9a15de64c2039
FaisalAli19 <faisalali1901@gmail.com>
2017-02-26 17:03:57 +0530

    fixed karma test issue

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,11 @@ module.exports = function(config) {
         browsers: ["Chrome"],
         singleRun: true,
         frameworks: ["mocha"],
-        files: ["App/tests/**/*.test.js"],
+        files: [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/foundation-sites/dist/js/foundation.min.js",
+            "App/tests/**/*.test.js"
+        ],
         preprocessors: {
             "App/tests/**/*.test.js": ["webpack", "sourcemap"]
         },
